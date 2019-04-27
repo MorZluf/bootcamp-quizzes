4cc8b3d1189658c1fcd0409bb7971b86aec79cd0
MorZluf <morzluf19@gmail.com>
2019-04-27 12:49:14 +0300

    answered week7's quizz

--- week5/answers.js
+++ week5/answers.js
@@ -6,20 +6,20 @@ const answers = {
     1: "b",
     2: "d",
     3: "b",
-    4: "a",
+    4: "a", //correct answer is e
     5: "c",
     6: "a",
-    7: "a",
-    8: "a",
+    7: "a", //correct answer is c
+    8: "a", //correct answer is b
     9: "c",
     10: "a",
-    11: "a",
-    12: "b",
-    13: "c",
+    11: "a", //correct answer is d
+    12: "b", //correct answer is e
+    13: "c", //correct answer is a
     14: "c",
     15: "b",
     16: "c",
     17: "a",
-    18: "a",
+    18: "a", //correct answer is b
     19: "b",
 }
